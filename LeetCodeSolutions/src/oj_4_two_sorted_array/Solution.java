dd53933ad110fce4088fd11b6448b99dd3cca76a
YuChenglong <yuchenglong8809@163.com>
2016-05-30 10:12:36 +0800

    leetcode oj 4:median of two sorted arrays 参考了如下帖子http://blog.csdn.net/yutianzuijin/article/details/11499917

---
+++ LeetCodeSolutions/src/oj_4_two_sorted_array/Solution.java
@@ -0,0 +1,50 @@
+package oj_4_two_sorted_array;
+
+/**
+ * 这道题实在有些难，参考了一下这帖子：http://blog.csdn.net/yutianzuijin/article/details/11499917
+ * 才做出来。
+ * 
+ * @author Prayer
+ *
+ */
+public class Solution {
+	/**
+	 * 
+	 * @param A
+	 * @param B
+	 * @return
+	 */
+	public double findMedianSortedArrays(int[] A, int[] B) {
+        // write your code here
+        int len = A.length + B.length;
+        if (len % 2 == 0) {
+            return (findKth(A, 0, B, 0, len/2) + findKth(A, 0, B, 0, len/2+1)) / 2.0;
+        } else {
+            return findKth(A, 0, B, 0, len/2 + 1);
+        }
+    }
+    
+    //find kth number of two sorted array
+    public static int findKth(int[] A, int A_start, int[] B, int B_start, int k) {
+        if (A_start >= A.length) {
+            return B[B_start + k - 1];
+        }
+        if (B_start >= B.length) {
+            return A[A_start + k - 1];
+        }
+        if (k == 1) {
+            return Math.min(A[A_start], B[B_start]);
+        }
+
+        int A_key = (A_start + k/2 - 1 < A.length) // if one array is too short
+                    ? A[A_start + k/2 - 1] : Integer.MAX_VALUE; // trick
+        int B_key = (B_start + k/2 - 1 < B.length) // if one array is too short
+                    ? B[B_start + k/2 - 1] : Integer.MAX_VALUE; // trick
+
+        if (A_key < B_key) {
+            return findKth(A, A_start + k/2, B, B_start, k - k/2);
+        } else {
+            return findKth(A, A_start, B, B_start + k/2, k - k/2);
+        }
+    }
+}
