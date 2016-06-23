3a29b73008c20b8f7bac61ff372288ec486ab44d
YuChenglong <yuchenglong8809@163.com>
2016-06-23 21:10:05 +0800

    思路

--- LeetCodeSolutions/src/oj_solution/oj_28_implement_strStr/Solution.java
+++ LeetCodeSolutions/src/oj_solution/oj_28_implement_strStr/Solution.java
@@ -3,6 +3,9 @@ package oj_solution.oj_28_implement_strStr;
 public class Solution {
 	public int strStr(String haystack, String needle) {
 		if(null == haystack || null == needle) return -1;
+		/*
+		 * TODO 其实没啥好的解法，直接遍历就是了，不过也许可以参考一下KMP算法？
+		 */
 		for(int i = 0; ; i++) {
 			for(int j = 0; ; j++){
 				if(j == needle.length()) return j;
