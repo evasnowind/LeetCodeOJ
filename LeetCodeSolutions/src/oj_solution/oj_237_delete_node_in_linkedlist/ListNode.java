94ec121357ba2183a8faef6575747a8d1dda66bf
yuchenglong <yuchenglong8809@163.com>
2016-07-21 19:50:33 +0800

    oj 237 delete node

---
+++ LeetCodeSolutions/src/oj_solution/oj_237_delete_node_in_linkedlist/ListNode.java
@@ -0,0 +1,7 @@
+package oj_solution.oj_237_delete_node_in_linkedlist;
+
+public class ListNode {
+	public int val;
+	public ListNode next;
+	public ListNode(int x){val = x;}
+}
---
+++ LeetCodeSolutions/src/oj_solution/oj_237_delete_node_in_linkedlist/Solution.java
@@ -0,0 +1,12 @@
+package oj_solution.oj_237_delete_node_in_linkedlist;
+
+public class Solution {
+	/*
+	 * 题目已说明node不会是尾节点，因此不用考虑node为空或是单节点情况
+	 * 一般思路：将后面一个节点拷贝过来，删除后面那个节点
+	 */
+	public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
