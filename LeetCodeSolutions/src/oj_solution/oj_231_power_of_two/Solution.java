674792ddba912fc73f965e0310600597277b8a70
yuchenglong <yuchenglong8809@163.com>
2016-08-10 21:46:07 +0800

    oj 231 power of two update

--- LeetCodeSolutions/src/oj_solution/oj_231_power_of_two/Solution.java
+++ LeetCodeSolutions/src/oj_solution/oj_231_power_of_two/Solution.java
@@ -2,6 +2,14 @@ package oj_solution.oj_231_power_of_two;
 
 public class Solution {
 	/*
+	 * 除了循环、递归，还可以考虑用位运算，2的乘方必然只有1个1、其他都是0（1，10，100，1000……），因此
+	 * 有如下解法：
+	 * 
+	 * bool isPowerOfTwo(int n) {
+        return (n>0&&((n&(n-1))==0));
+    	}
+	 */
+	/*
 	 * 注意考虑0 、1等特殊情况
 	 */
 	public boolean isPowerOfTwo(int n) {
