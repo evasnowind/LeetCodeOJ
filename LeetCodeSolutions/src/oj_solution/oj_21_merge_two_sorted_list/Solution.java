13fea14dcd0490a2f6f6ded4854eb715ecbf4677
yuchenglong <yuchenglong8809@163.com>
2016-08-13 15:01:53 +0800

    oj 21 merge two sorted list(modified)

--- LeetCodeSolutions/src/oj_solution/oj_21_merge_two_sorted_list/Solution.java
+++ LeetCodeSolutions/src/oj_solution/oj_21_merge_two_sorted_list/Solution.java
@@ -10,24 +10,24 @@ public class Solution {
         p1 = l1;
         p2 = l2;
         cur = dummy;
-        while(null != p1 || null != p2){
-        	if(null == p1){
-        		while(null != p2){
-        			cur.next = p2;
-        			p2 = p2.next;
-        			cur = cur.next;
-        		}
-        	}
-        	if(null == p2){
-        		while(null != p1){
-        			cur.next = p1;
-        			p1 = p1.next;
-        			cur = cur.next;
-        		}
-        	}
-        	
-        	
-        }
+    	while(null != p1 && null != p2){
+    		if(p1.val <= p2.val){
+    			cur.next = p1;
+    			cur = p1;
+    			p1 = p1.next;
+    		} else {
+    			cur.next = p2;
+    			cur = p2;
+    			p2 = p2.next;
+    		}
+    	}
+    	if(null == p1){
+    		cur.next = p2;
+    	}
+    	if(null == p2){
+    		cur.next = p1;
+    	}
+       
         return dummy.next;
     }
 }
