33000d3433ccdcd2cfa851cea45d91d56ff180ed
yuchenglong <yuchenglong8809@163.com>
2016-08-26 20:43:19 +0800

    oj 268 missing number

---
+++ LeetCodeSolutions/src/oj_solution/oj_268_missing_number/Solution.java
@@ -0,0 +1,15 @@
+package oj_solution.oj_268_missing_number;
+
+public class Solution {
+	/*
+	 * 基本思路：用一个boolean数组，遍历2次，第一次将nums数组中所有出现的数
+	 * 			在boolean数组对应位置置为true，第二次遍历boolean数组即可
+	 * 			O(n) 时间  O(n)空间
+	 * 基本思路2：用一个数字进行位运算来代替boolean数组，以保存标志位
+	 * 基本思路3：排序后遍历，看不连续的位置	O(nlogn) 时间  O(1)空间
+	 * 
+	 */
+	public int missingNumber(int[] nums) {
+		return 0;
+    }
+}
