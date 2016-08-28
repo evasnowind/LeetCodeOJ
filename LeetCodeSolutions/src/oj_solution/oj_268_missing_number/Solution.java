012f1cf9650138276fc84c69d31e56c7fca5044a
yuchenglong <yuchenglong8809@163.com>
2016-08-28 17:17:54 +0800

    oj 268 missing number #interesting

--- LeetCodeSolutions/src/oj_solution/oj_268_missing_number/Solution.java
+++ LeetCodeSolutions/src/oj_solution/oj_268_missing_number/Solution.java
@@ -18,6 +18,10 @@ public class Solution {
 	 * 
 	 */
 	public int missingNumber(int[] nums) {
-		return 0;
+		int xor = 0, i = 0;
+    	for (i = 0; i < nums.length; i++) {
+    		xor = xor ^ i ^ nums[i];
+    	}
+    	return xor ^ i;
     }
 }
