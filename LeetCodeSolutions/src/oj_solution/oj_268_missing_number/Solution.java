cf2e12d308675acb222f41c27b4a7cad8dea1344
yuchenglong <yuchenglong8809@163.com>
2016-08-31 10:45:01 +0800

    update oj 268 comment

--- LeetCodeSolutions/src/oj_solution/oj_268_missing_number/Solution.java
+++ LeetCodeSolutions/src/oj_solution/oj_268_missing_number/Solution.java
@@ -10,12 +10,10 @@ public class Solution {
 	 * 基本思路3：排序后遍历，看不连续的位置	O(nlogn) 时间  O(1)空间
 	 * 优化后思路：参见
 	 * 
-	 * https://discuss.leetcode.com/topic/24535/4-line-simple-java-bit-manipulate-solution-with-explaination/2
-	 * 
-	 * https://discuss.leetcode.com/topic/24535/4-line-simple-java-bit-manipulate-solution-with-explaination/5
-	 * 
-	 * https://discuss.leetcode.com/topic/22313/c-solution-using-bit-manipulation
-	 * 
+	 * 思路1：用XOR 依据： a^b^b =a 由于少一个数字，遍历完后还需要^i。
+	 * 思路2：将[0,n]的数加起来（或者直接用等比数列公式算也可以），然后减去数组中的n-1个数字，最后即为所得。
+	 *
+	 * https://discuss.leetcode.com/topic/24535/4-line-simple-java-bit-manipulate-solution-with-explaination/
 	 */
 	public int missingNumber(int[] nums) {
 		int xor = 0, i = 0;
