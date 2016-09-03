d83ee73b9b22bc4d81a670f8ef1469796d7894bc
yuchenglong <yuchenglong8809@163.com>
2016-09-03 20:51:00 +0800

    oj 187 repeated dns seq #to be continue

---
+++ LeetCodeSolutions/src/oj_solution/oj_187_repeated_dns_sequences/Solution.java
@@ -0,0 +1,9 @@
+package oj_solution.oj_187_repeated_dns_sequences;
+
+import java.util.List;
+
+public class Solution {
+	public List<String> findRepeatedDnaSequences(String s) {
+        return null;
+    }
+}
