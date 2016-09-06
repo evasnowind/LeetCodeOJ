be864c9983415ba3413fcbac1247a8697df6c6ea
yuchenglong <yuchenglong8809@163.com>
2016-09-06 19:32:07 +0800

    oj 121 to be continue

---
+++ LeetCodeSolutions/src/oj_solution/oj_121_best_time_to_buy_and_sell_stock/Solution.java
@@ -0,0 +1,10 @@
+package oj_solution.oj_121_best_time_to_buy_and_sell_stock;
+
+public class Solution {
+	public int maxProfit(int[] prices) {
+		int res = 0;
+		if(null == prices) return res;
+		
+        return 0;
+    }
+}
