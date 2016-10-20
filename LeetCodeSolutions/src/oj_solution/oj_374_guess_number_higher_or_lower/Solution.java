7b357054d60d5d4ffc9544ef15e95ac72b802427
yuchenglong <yuchenglong8809@163.com>
2016-10-20 21:14:49 +0800

    update oj 374 comment

--- LeetCodeSolutions/src/oj_solution/oj_374_guess_number_higher_or_lower/Solution.java
+++ LeetCodeSolutions/src/oj_solution/oj_374_guess_number_higher_or_lower/Solution.java
@@ -1,6 +1,10 @@
 package oj_solution.oj_374_guess_number_higher_or_lower;
 
 public class Solution extends GuessGame{
+	/*
+	 * 分析：主要是要注意看清题目，guessNumber的用意是猜出某个[1,n]范围内的值，
+	 * 		guess仅仅是一个判断函数，那么直接二分查找即可
+	 */
 	public int guessNumber(int n) {
         int i = 1, j = n;
         while(i < j) {
