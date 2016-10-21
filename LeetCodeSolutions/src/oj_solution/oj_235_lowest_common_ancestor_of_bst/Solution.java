247b7d700f02247b8095601557bdb6d0db830de6
yuchenglong <yuchenglong8809@163.com>
2016-10-21 22:09:56 +0800

    oj 235 lowest common ancestor of binary search tree

---
+++ LeetCodeSolutions/src/oj_solution/oj_235_lowest_common_ancestor_of_bst/Solution.java
@@ -0,0 +1,9 @@
+package oj_solution.oj_235_lowest_common_ancestor_of_bst;
+
+import oj_solution.oj_226_invert_binary_tree.TreeNode;
+
+public class Solution {
+	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        return null;
+    }
+}
