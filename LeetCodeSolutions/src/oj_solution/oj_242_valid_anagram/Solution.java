5894cd3f6f480ed74a6ee030e6e4ae3d392ce5c4
yuchenglong <yuchenglong8809@163.com>
2016-10-25 21:04:49 +0800

    oj 242 valid anagram

--- LeetCodeSolutions/src/oj_solution/oj_242_valid_anagram/Solution.java
+++ LeetCodeSolutions/src/oj_solution/oj_242_valid_anagram/Solution.java
@@ -1,5 +1,21 @@
 package oj_solution.oj_242_valid_anagram;
 
 public class Solution {
-
+	/*
+	 * 分析：注意看清题目即可，部分字母颠倒顺序，并不一定是全部颠倒。
+	 * 		若是unicode的情况，使用hash表即可
+	 */
+	public boolean isAnagram(String s, String t) {
+		if(null == s && null == t) return true;
+		if(null != s && null != t){
+			if(s.length() != t.length()) return false;
+			int[] alphabet = new int[26];
+	        for (int i = 0; i < s.length(); i++) alphabet[s.charAt(i) - 'a']++;
+	        for (int i = 0; i < t.length(); i++) alphabet[t.charAt(i) - 'a']--;
+	        for (int i : alphabet) if (i != 0) return false;
+	        return true;
+		} else {
+			return false;
+		}
+    }
 }
