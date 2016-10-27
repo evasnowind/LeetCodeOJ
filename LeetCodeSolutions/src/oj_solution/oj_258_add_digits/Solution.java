30186c26d6272ab7d1d883f69aac7f42b9dd13bc
yuchenglong <yuchenglong8809@163.com>
2016-10-27 20:37:51 +0800

    update oj 258 comment

--- LeetCodeSolutions/src/oj_solution/oj_258_add_digits/Solution.java
+++ LeetCodeSolutions/src/oj_solution/oj_258_add_digits/Solution.java
@@ -2,9 +2,14 @@ package oj_solution.oj_258_add_digits;
 
 public class Solution {
 	/*
+	 * 
+	 * 如果用直观思路，直接按照题目要求计算，很容易写出算法，但运行时间较长
+	 * 
+	 * 但本题是经典的digital root问题，参见https://en.wikipedia.org/wiki/Digital_root
+	 * 
 	 * https://discuss.leetcode.com/topic/21498/accepted-c-o-1-time-o-1-space-1-line-solution-with-detail-explanations
-	 * 经典的digital root问题，参见https://en.wikipedia.org/wiki/Digital_root
-	 * 对于
+	 * 
+	 * 
 	 * For base b (decimal case b = 10), the digit root of an integer is:
 
 			dr(n) = 0 if n == 0
@@ -24,8 +29,9 @@ output: 0 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 ....
 
 Henceforth, we can write the following code, whose time and space complexities are both O(1).
 
-	针对十进制 dr(n) = 1 + (n - 1) % 9
-
+	使用上面公式，对于十进制，
+	   dr(n) = 1 + (n - 1) % 9
+	
 	 */
 	
 	public int addDigits(int num) {
