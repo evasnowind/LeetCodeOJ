cc500ea80abdb830b74ae1368010a7a6263e5c5a
yuchenglong <yuchenglong8809@163.com>
2016-10-29 18:29:42 +0800

    oj 105 init commit

---
+++ LeetCodeSolutions/src/oj_solution/oj_105_contruct_binary_tree/Solution.java
@@ -0,0 +1,9 @@
+package oj_solution.oj_105_contruct_binary_tree;
+
+import oj_solution.oj_226_invert_binary_tree.TreeNode;
+
+public class Solution {
+	public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return null;
+    }
+}
