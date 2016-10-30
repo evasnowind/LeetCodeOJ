009a0c7bac31b62a8e373f4e18106f35f5b9c616
yuchenglong <yuchenglong8809@163.com>
2016-10-30 21:15:23 +0800

    oj 114 init commit

---
+++ LeetCodeSolutions/src/oj_solution/oj_114_flattern_bt_to_linked_list/Solution.java
@@ -0,0 +1,9 @@
+package oj_solution.oj_114_flattern_bt_to_linked_list;
+
+import oj_solution.oj_226_invert_binary_tree.TreeNode;
+
+public class Solution {
+	public void flatten(TreeNode root) {
+        
+    }
+}
