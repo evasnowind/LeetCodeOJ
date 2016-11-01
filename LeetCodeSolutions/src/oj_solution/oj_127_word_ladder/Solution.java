2ad71567943294bc5c49db8c65bd01f4b398e4f6
yuchenglong <yuchenglong8809@163.com>
2016-11-01 18:34:02 +0800

    oj 127 init commit

---
+++ LeetCodeSolutions/src/oj_solution/oj_127_word_ladder/Solution.java
@@ -0,0 +1,9 @@
+package oj_solution.oj_127_word_ladder;
+
+import java.util.Set;
+
+public class Solution {
+	public int ladderLength(String beginWord, String endWord, Set<String> wordList) {
+        return 0;
+    }
+}
