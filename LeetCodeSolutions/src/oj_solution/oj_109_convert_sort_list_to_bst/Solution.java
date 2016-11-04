78d53cbf1a5a2b162114b3708ce96f29ed66215d
yuchenglong <yuchenglong8809@163.com>
2016-11-04 19:13:49 +0800

    oj 109 init commit

---
+++ LeetCodeSolutions/src/oj_solution/oj_109_convert_sort_list_to_bst/Solution.java
@@ -0,0 +1,10 @@
+package oj_solution.oj_109_convert_sort_list_to_bst;
+
+import oj_solution.oj_226_invert_binary_tree.TreeNode;
+import oj_solution.oj_24_swap_nodes_in_pairs.ListNode;
+
+public class Solution {
+	public TreeNode sortedListToBST(ListNode head) {
+        return null;
+    }
+}
