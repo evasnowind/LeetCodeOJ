17120294474ca5f5ea918392c4888638e04a5bfc
yuchenglong <yuchenglong8809@163.com>
2016-11-06 21:52:47 +0800

    oj 131 palindrome partitioning

---
+++ LeetCodeSolutions/src/oj_solution/oj_131_palindrome_partitioning/Solution.java
@@ -0,0 +1,9 @@
+package oj_solution.oj_131_palindrome_partitioning;
+
+import java.util.List;
+
+public class Solution {
+	public List<List<String>> partition(String s) {
+        return null;
+    }
+}
