8c69f96572237ab8a5a0eeb2fadfc9077453eb2f
yuchenglong <yuchenglong8809@163.com>
2016-11-07 23:16:21 +0800

    oj 106 commit

---
+++ LeetCodeSolutions/src/oj_solution/oj_106_contruct_bt_from_inorder_postorder/Solution.java
@@ -0,0 +1,9 @@
+package oj_solution.oj_106_contruct_bt_from_inorder_postorder;
+
+import oj_solution.oj_226_invert_binary_tree.TreeNode;
+
+public class Solution {
+	public TreeNode buildTree(int[] inorder, int[] postorder) {
+		return null;
+    }
+}
