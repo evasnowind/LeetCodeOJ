d701135d559c563881f48d4b82d205394aed8499
yuchenglong <yuchenglong8809@163.com>
2016-11-11 16:54:41 +0800

    oj 116 init commit

---
+++ LeetCodeSolutions/src/oj_solution/oj_116_populating_next_right_pointer_in_each_node/Solution.java
@@ -0,0 +1,7 @@
+package oj_solution.oj_116_populating_next_right_pointer_in_each_node;
+
+public class Solution {
+	public void connect(TreeLinkNode root) {
+        
+    }
+}
---
+++ LeetCodeSolutions/src/oj_solution/oj_116_populating_next_right_pointer_in_each_node/TreeLinkNode.java
@@ -0,0 +1,7 @@
+package oj_solution.oj_116_populating_next_right_pointer_in_each_node;
+
+public class TreeLinkNode {
+	public int val;
+	public TreeLinkNode left, right, next;
+	public TreeLinkNode(int x) { val = x; }
+}
