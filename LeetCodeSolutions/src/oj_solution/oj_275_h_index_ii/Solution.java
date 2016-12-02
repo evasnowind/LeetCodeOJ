7a378afb40482f2ce48b463d2c8dca03b84ad095
yuchenglong <yuchenglong8809@163.com>
2016-12-02 15:07:45 +0800

    oj 275 h index ii

--- LeetCodeSolutions/src/oj_solution/oj_275_h_index_ii/Solution.java
+++ LeetCodeSolutions/src/oj_solution/oj_275_h_index_ii/Solution.java
@@ -1,5 +1,25 @@
 package oj_solution.oj_275_h_index_ii;
 
 public class Solution {
-
+	/**
+	 * 分析：输入已经排好序，很容易让人想到需要用到
+	 * 		二分查找的思想
+	 * @param citations
+	 * @return
+	 */
+	public int hIndex(int[] citations) {
+		if(null == citations || citations.length == 0) return 0;
+		int len = citations.length, left = 0, right = len - 1;
+		while(left <= right){
+			int mid = left + (right - left) / 2;
+			if(citations[mid] == len - mid){
+				return len - mid;
+			} else if(citations[mid] > len - mid){
+				right = mid - 1;
+			} else {
+				left = mid + 1;
+			}
+		}
+		return len - left;
+    }
 }
