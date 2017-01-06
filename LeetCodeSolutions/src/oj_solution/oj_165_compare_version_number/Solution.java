9e816ebe749d70c3e434cee158a37e730a47beea
yuchenglong <yuchenglong8809@163.com>
2017-01-06 22:56:29 +0800

    oj 165 compare version number

---
+++ LeetCodeSolutions/src/oj_solution/oj_165_compare_version_number/Solution.java
@@ -0,0 +1,27 @@
+package oj_solution.oj_165_compare_version_number;
+
+public class Solution {
+	/**
+	 * 本题其实关键在于弄清楚输入数据的结构、版本号比较的逻辑，考虑好各种边界条件
+	 * 
+	 * @param version1
+	 * @param version2
+	 * @return
+	 */
+	public int compareVersion(String version1, String version2) {
+		String[] levels1 = version1.split("\\.");
+	    String[] levels2 = version2.split("\\.");
+	    
+	    int length = Math.max(levels1.length, levels2.length);
+	    for (int i=0; i<length; i++) {
+	    	Integer v1 = i < levels1.length ? Integer.parseInt(levels1[i]) : 0;
+	    	Integer v2 = i < levels2.length ? Integer.parseInt(levels2[i]) : 0;
+	    	int compare = v1.compareTo(v2);
+	    	if (compare != 0) {
+	    		return compare;
+	    	}
+	    }
+	    
+	    return 0;
+    }
+}
