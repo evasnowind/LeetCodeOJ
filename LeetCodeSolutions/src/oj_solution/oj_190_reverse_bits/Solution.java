b3620fae6d9732528a1f0a621c8963ae02c41fbc
yuchenglong <yuchenglong8809@163.com>
2017-01-31 15:36:40 +0800

    update oj 190 reverse bits code

--- LeetCodeSolutions/src/oj_solution/oj_190_reverse_bits/Solution.java
+++ LeetCodeSolutions/src/oj_solution/oj_190_reverse_bits/Solution.java
@@ -23,7 +23,7 @@ public class Solution {
     	int res = 0;
     	for(int i = 0; i < 32; i++){
     		res = res << 1;
-    		res += n & 1;
+    		res |= n & 1;
     		n = n >>> 1;
     	}
     	return res;
