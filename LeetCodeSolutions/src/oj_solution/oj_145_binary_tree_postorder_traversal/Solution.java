3e9a20a49281a79c9d680f6d743e4c566743ea6b
yuchenglong <yuchenglong8809@163.com>
2017-02-16 20:58:28 +0800

    oj 145 binary tree postorder traversal..

---
+++ LeetCodeSolutions/src/oj_solution/oj_145_binary_tree_postorder_traversal/Solution.java
@@ -0,0 +1,38 @@
+package oj_solution.oj_145_binary_tree_postorder_traversal;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+import oj_solution.oj_226_invert_binary_tree.TreeNode;
+
+public class Solution {
+	/*
+	 * 后序遍历的非递归实现是先/中/后序非递归中最难的
+	 */
+	public List<Integer> postorderTraversal(TreeNode root) {
+		List<Integer> res = new ArrayList<Integer>();
+		if(null == root) return res;
+		Stack<TreeNode> nodeStack = new Stack<TreeNode>();
+		TreeNode curNode = root, lastNode = null;
+		while(null != curNode){
+			nodeStack.push(curNode);
+			curNode = curNode.left;
+		}
+		while(!nodeStack.isEmpty()){
+			curNode = nodeStack.pop();
+			if(null == curNode.right || curNode.right == lastNode){
+				res.add(curNode.val);
+				lastNode = curNode;
+			} else {
+				nodeStack.push(curNode);
+				curNode = curNode.right;
+				while(null != curNode){
+					nodeStack.push(curNode);
+					curNode = curNode.left;
+				}
+			}
+		}
+		return res;
+    }
+}
