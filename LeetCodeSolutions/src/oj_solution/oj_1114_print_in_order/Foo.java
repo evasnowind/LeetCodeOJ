7b4f6c53ae76d0bbbcde7ab9ca0fe88288daae6b
Chenglong Yu <chenglong.yu@100credit.com>
2019-09-25 22:24:27 +0800

    oj 220 contains duplicate iii

---
+++ LeetCodeSolutions/src/oj_solution/oj_1114_print_in_order/Foo.java
@@ -0,0 +1,29 @@
+package oj_1114_print_in_order;
+
+/**
+ * @author chenglong.yu@100credit.com
+ * created on 2019/9/14
+ */
+public class Foo {
+    public Foo() {
+
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+
+        // printFirst.run() outputs "first". Do not change or remove this line.
+        printFirst.run();
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        printSecond.run();
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+
+        // printThird.run() outputs "third". Do not change or remove this line.
+        printThird.run();
+    }
+}
