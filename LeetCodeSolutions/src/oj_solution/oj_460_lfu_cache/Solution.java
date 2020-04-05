1cd0631b8c0b7a647b97511518fee316e5248f46
Chenglong Yu <chenglong.yu@100credit.com>
2020-04-05 21:19:46 +0800

    oj 460 LFU

---
+++ LeetCodeSolutions/src/oj_solution/oj_460_lfu_cache/Solution.java
@@ -0,0 +1,112 @@
+package oj_460_lfu_cache;
+
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+
+/**
+ * @author chenglong.yu@100credit.com
+ * created on 2020/4/5
+ */
+public class LFUCache {
+    Map<Integer, Node> cache;  // 存储缓存的内容
+    Map<Integer, LinkedHashSet<Node>> freqMap; // 存储每个频次对应的双向链表
+    int size;
+    int capacity;
+    int min; // 存储当前最小频次
+
+    /**
+     * 参见https://leetcode-cn.com/problems/lfu-cache/solution/lfuhuan-cun-by-leetcode-solution/
+     * 官方题解
+     * 基本思路：两个map，1个map保存<key,value>缓存数据，另一个保存每个key对应的频率
+     * <key, list>，因为同一个频次的数据可能有多个
+     * 每次put/get都需要更新频次map。另外用一个min变量来保存当前最少的查询次数，以便后续
+     * put时发现空间不够、需要将最近最少使用的缓存数据无效
+     *
+     * @param capacity
+     */
+    public LFUCache(int capacity) {
+        cache = new HashMap<> (capacity);
+        freqMap = new HashMap<>();
+        this.capacity = capacity;
+    }
+
+    public int get(int key) {
+        Node node = cache.get(key);
+        if (node == null) {
+            return -1;
+        }
+        freqInc(node);
+        return node.value;
+    }
+
+    public void put(int key, int value) {
+        if (capacity == 0) {
+            return;
+        }
+        Node node = cache.get(key);
+        if (node != null) {
+            node.value = value;
+            freqInc(node);
+        } else {
+            if (size == capacity) {
+                Node deadNode = removeNode();
+                cache.remove(deadNode.key);
+                size--;
+            }
+            Node newNode = new Node(key, value);
+            cache.put(key, newNode);
+            addNode(newNode);
+            size++;
+        }
+    }
+
+    void freqInc(Node node) {
+        // 从原freq对应的链表里移除, 并更新min
+        int freq = node.freq;
+        LinkedHashSet<Node> set = freqMap.get(freq);
+        set.remove(node);
+        if (freq == min && set.size() == 0) {
+            min = freq + 1;
+        }
+        // 加入新freq对应的链表
+        node.freq++;
+        LinkedHashSet<Node> newSet = freqMap.get(freq + 1);
+        if (newSet == null) {
+            newSet = new LinkedHashSet<>();
+            freqMap.put(freq + 1, newSet);
+        }
+        newSet.add(node);
+    }
+
+    void addNode(Node node) {
+        LinkedHashSet<Node> set = freqMap.get(1);
+        if (set == null) {
+            set = new LinkedHashSet<>();
+            freqMap.put(1, set);
+        }
+        set.add(node);
+        min = 1;
+    }
+
+    Node removeNode() {
+        LinkedHashSet<Node> set = freqMap.get(min);
+        Node deadNode = set.iterator().next();
+        set.remove(deadNode);
+        return deadNode;
+    }
+}
+
+class Node {
+    int key;
+    int value;
+    int freq = 1;
+
+    public Node() {}
+
+    public Node(int key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+}
+
