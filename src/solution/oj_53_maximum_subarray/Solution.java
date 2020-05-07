731e5d75860c51db54218dcdfec87cd2f0831f65
Chenglong Yu <chenglong.yu@100credit.com>
2020-05-07 13:26:16 +0800

    临时保存代码，涉及 oj 617, 572, 53

---
+++ src/solution/oj_53_maximum_subarray/Solution.java
@@ -0,0 +1,9 @@
+package solution.oj_53_maximum_subarray;
+
+/**
+ * @author chenglong.yu@100credit.com
+ * created on 2020/5/3
+ */
+public class Solution {
+
+}
---
+++ src/solution/oj_572_subtree_of_another_tree/Solution.java
@@ -0,0 +1,28 @@
+package solution.oj_572_subtree_of_another_tree;
+
+/**
+ * @author chenglong.yu@100credit.com
+ * created on 2020/5/7
+ */
+public class Solution {
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
