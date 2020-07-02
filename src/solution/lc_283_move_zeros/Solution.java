681a464888dcf807e5bf29c2e3180bf8014c9e4c
Chenglong Yu <chenglong.yu@100credit.com>
2020-07-02 13:53:58 +0800

    leetcode explore 题目重刷

--- src/solution/lc_283_move_zeros/Solution.java
+++ src/solution/lc_283_move_zeros/Solution.java
@@ -25,6 +25,33 @@ public class Solution {
 		}
     }
 
+	public static void moveZeroes2(int[] nums) {
+		int lastZeroFoundAt = 0;
+		int tmp = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] != 0) {
+				/*
+				lastZeroFoundAt将保存上次发现的0所在位置，将该位置替换为i所指的最新非零值，然后lastZeroFoundAt+1。
+				lastZeroFoundAt新位置若不是0，i和lastZeroFoundAt会相等，于是i和lastZeroFoundAt都会继续加1，直至
+				lastZeroFoundAt所指的位置是0。
+				 */
+
+				tmp = nums[i];
+				nums[i] = nums[lastZeroFoundAt];
+				nums[lastZeroFoundAt] = tmp;
+				lastZeroFoundAt += 1;
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		int[] nums = new int[]{0,1,0,3,12};
+
+//		moveZeroes2(nums);
+		nums = new int[]{1,2,0,3,12};
+		moveZeroes2(nums);
+	}
+
     /*
     在上面基础之上，进一步优化，不用拷贝，直接交换当前0、与后面的非0元素，这样可以避免第2个循环，代码如下：
     class Solution {
