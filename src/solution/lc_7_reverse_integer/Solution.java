ab180c309642472c00e1ca43a96144291fda4d35
Chenglong Yu <chenglong.yu@100credit.com>
2020-07-04 20:13:09 +0800

    leetcode 7 reverse integer

--- src/solution/lc_7_reverse_integer/Solution.java
+++ src/solution/lc_7_reverse_integer/Solution.java
@@ -28,5 +28,52 @@ The input is assumed to be a 32-bit signed integer. Your function should return 
         
         return sign * result;
     }
+
+
+    /*
+    为了便于解释，我们假设 \text{rev}rev 是正数。
+
+如果 temp = \text{rev} \cdot 10 + \text{pop}temp=rev⋅10+pop 导致溢出，那么一定有 \text{rev} \geq \frac{INTMAX}{10}rev≥
+10
+INTMAX
+​
+ 。
+如果 \text{rev} > \frac{INTMAX}{10}rev>
+10
+INTMAX
+​
+ ，那么 temp = \text{rev} \cdot 10 + \text{pop}temp=rev⋅10+pop 一定会溢出。
+如果 \text{rev} == \frac{INTMAX}{10}rev==
+10
+INTMAX
+​
+ ，那么只要 \text{pop} > 7pop>7，temp = \text{rev} \cdot 10 + \text{pop}temp=rev⋅10+pop 就会溢出。
+
+作者：LeetCode
+链接：https://leetcode-cn.com/problems/reverse-integer/solution/zheng-shu-fan-zhuan-by-leetcode/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    public int reverse2(int x) {
+		int rev = 0;
+		while (x != 0) {
+			int pop = x % 10;
+			x /= 10;
+			if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
+			if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
+			rev = rev * 10 + pop;
+		}
+		return rev;
+	}
+
+	public static void main(String[] args) {
+		Solution s = new Solution();
+		int x = -11;
+		System.out.println(x % 10);
+		System.out.println(x / 10);
+		System.out.println(s.reverse2(x));
+		System.out.println(s.reverse2(-122));
+		System.out.println(s.reverse2(234));
+	}
 }
  
