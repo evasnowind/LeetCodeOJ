061aaf8bb375e0a2f8985c900c036872b6825764
Chenglong Yu <chenglong.yu@100credit.com>
2020-08-14 20:17:03 +0800

    offer problems: 10 fibonacci

--- src/solution/offer_problem/p10_i_fibonacci/Solution.java
+++ src/solution/offer_problem/p10_i_fibonacci/Solution.java
@@ -2,4 +2,17 @@ package solution.offer_problem.p10_i_fibonacci;
 
 public class Solution {
 
+
+    public int fib(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        int f0 = 0, f1 = 1, res = 0;
+        for(int i = 2; i <= n; i++) {
+            res = (f0 + f1) % 1000000007;
+            f0 = f1;
+            f1 = res;
+        }
+        return res;
+    }
 }
--- src/solution/offer_problem/p10_ii_climb_stairs/Solution.java
+++ src/solution/offer_problem/p10_ii_climb_stairs/Solution.java
@@ -2,4 +2,27 @@ package solution.offer_problem.p10_ii_climb_stairs;
 
 public class Solution {
 
+    public int numWays(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+
+        /*
+        青蛙跳台阶问题： f(0)=1 , f(1)=1 , f(2)=2 ；
+        斐波那契数列问题： f(0)=0 , f(1)=1, f(2)=1 。
+
+        作者：jyd
+        链接：https://leetcode-cn.com/problems/qing-wa-tiao-tai-jie-wen-ti-lcof/solution/mian-shi-ti-10-ii-qing-wa-tiao-tai-jie-wen-ti-dong/
+
+         */
+        //此处为了减少一次if判断n=2，直接将f2赋值给res。
+        int f1 = 1, f2 = 2, res = f2;
+        for (int i = 3; i <= n; i++) {
+            res = (f1 + f2) % 1000000007;
+            f1 = f2;
+            f2 = res;
+        }
+
+        return res;
+    }
 }
