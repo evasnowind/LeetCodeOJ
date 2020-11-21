18778e172f9fb50d80c2d1d94536ead7a6ce9afa
Chenglong Yu <chenglong.yu@100credit.com>
2020-11-21 15:01:17 +0800

    update leetcode 1111 max depth after split

--- src/solution/leetcode/lc_1111_max_depth_after_split/Solution.java
+++ src/solution/leetcode/lc_1111_max_depth_after_split/Solution.java
@@ -20,4 +20,28 @@ public class Solution {
 
         return ans;
     }
+
+    /**
+     * 计算嵌套深度时进行区分。
+     * 计算嵌套过程是用栈，但此处只需要计算出深度，深度是奇数分到一组，深度是偶数分到另一组，
+     * 可以不用栈、直接用变量保存当前栈深度的计数。只是需要注意一下d加1、减1的时机
+     * 这样比较容易理解。上面的实现是总结规律出来的，估计比较难理解自己想出来。
+     * @param seq
+     * @return
+     */
+    public int[] maxDepthAfterSplit2(String seq) {
+        int d = 0;
+        int length = seq.length();
+        int[] ans = new int[length];
+        for (int i = 0; i < length; i++) {
+            if (seq.charAt(i) == '(') {
+                ++d;
+                ans[i] = d % 2;
+            } else {
+                ans[i] = d % 2;
+                --d;
+            }
+        }
+        return ans;
+    }
 }
