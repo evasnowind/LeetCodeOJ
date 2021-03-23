98081c19f29b9534fa202aef8e8f83fd1750f2da
Chenglong Yu <yuchenglong8809@163.com>
2021-03-23 23:05:20 +0800

    lc 456 132 pattern

---
+++ src/solution/leetcode/lc_456_132_pattern/Solution.java
@@ -0,0 +1,53 @@
+package solution.leetcode.lc_456_132_pattern;
+
+import java.util.TreeMap;
+
+/**
+ * @author chenglong.yu
+ * created on 2021/3/24
+ */
+public class Solution {
+
+    /**
+     *
+     作者：LeetCode-Solution
+     链接：https://leetcode-cn.com/problems/132-pattern/solution/132mo-shi-by-leetcode-solution-ye89/
+     来源：力扣（LeetCode）
+     著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+     * @param nums
+     * @return
+     */
+    public boolean find132pattern(int[] nums) {
+        int n = nums.length;
+        if (n < 3) {
+            return false;
+        }
+
+        // 左侧最小值
+        int leftMin = nums[0];
+        // 右侧所有元素
+        TreeMap<Integer, Integer> rightAll = new TreeMap<Integer, Integer>();
+
+        for (int k = 2; k < n; ++k) {
+            rightAll.put(nums[k], rightAll.getOrDefault(nums[k], 0) + 1);
+        }
+
+        for (int j = 1; j < n - 1; ++j) {
+            if (leftMin < nums[j]) {
+                Integer next = rightAll.ceilingKey(leftMin + 1);
+                if (next != null && next < nums[j]) {
+                    return true;
+                }
+            }
+            leftMin = Math.min(leftMin, nums[j]);
+            rightAll.put(nums[j + 1], rightAll.get(nums[j + 1]) - 1);
+            if (rightAll.get(nums[j + 1]) == 0) {
+                rightAll.remove(nums[j + 1]);
+            }
+        }
+
+        return false;
+    }
+
+}
